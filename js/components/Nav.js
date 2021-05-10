1da0a28a9eeef1167edd677593fd63115294dcf9
Rimantas <belauzas@yahoo.com>
2021-05-10 16:13:10 +0300

    Nav content

--- js/components/Nav.js
+++ js/components/Nav.js
@@ -1,5 +1,36 @@
 class Nav {
+    constructor(selector, data) {
+        this.selector = selector;
+        this.data = data;
+    }
 
+    init() {
+        if (!this.isValidSelector() ||
+            !this.isValidData()) {
+            return false;
+        }
+
+        this.DOM = document.querySelector(this.selector);
+        if (!this.DOM) {
+            return false;
+        }
+
+        this.render();
+    }
+
+    isValidSelector() {
+        return true;
+    }
+
+    isValidData() {
+        return true;
+    }
+
+    render() {
+        let HTML = '';
+
+        this.DOM.innerHTML = HTML;
+    }
 }
 
 export { Nav }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { Languages } from './components/Languages.js';
 import { languagesData } from './data/languagesData.js';
 
+import { Nav } from './components/Nav.js';
+
 new Languages('#lang_switch', languagesData);
+new Nav('nav', null);
